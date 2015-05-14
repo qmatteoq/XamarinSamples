0de5c248817bdc1aacc520fbbf52ed1b8e2fabcb
unknown <mpagani@MPAGANI-PRO3.europe.corp.microsoft.com>
2015-05-14 16:28:01 +0200

    Added Android Fundamentals sample

---
+++ AndroidFundamentals/AndroidFundamentals/CallDetailActivity.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace AndroidFundamentals
+{
+    [Activity(Label = "CallDetailActivity")]
+    public class CallDetailActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Detail);
+
+            IList<string> parameters = Intent.Extras.GetStringArrayList("phone_numbers") ?? new string[0];
+            string phoneNumber = parameters[0];
+
+            TextView phoneNumberView = this.FindViewById<TextView>(Resource.Id.PhoneNumber);
+            Button callButton = this.FindViewById<Button>(Resource.Id.Call);
+
+            phoneNumberView.Text = phoneNumber;
+
+            callButton.Click += (sender, args) =>
+            {
+                var callIntent = new Intent(Intent.ActionCall);
+                callIntent.SetData(Android.Net.Uri.Parse("tel:" + phoneNumber));
+                StartActivity(callIntent);
+            };
+        }
+    }
+}
---
+++ AndroidFundamentals/AndroidFundamentals/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidFundamentals
+{
+    [Activity(Label = "AndroidFundamentals", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button callButton = this.FindViewById<Button>(Resource.Id.CallButton);
+            EditText phoneNumberText = this.FindViewById<EditText>(Resource.Id.PhoneNumberText);
+
+            callButton.Click += (sender, args) =>
+            {
+                var intent = new Intent(this, typeof(CallDetailActivity));
+                intent.PutStringArrayListExtra("phone_numbers", new[] { phoneNumberText.Text});
+                this.StartActivity(intent);
+            };
+        }
+    }
+}
+

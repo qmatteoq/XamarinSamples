1861076da816b0c9b142613b2f097e35960430de
Matteo Pagani <info@qmatteoq.com>
2015-05-15 01:13:47 +0200

    Added CustomAdapter demo

---
+++ Android/CustomListAdapter/CharacterAdapter.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Koush;
+using MarvelPortable.Model;
+
+namespace CustomListAdapter
+{
+    public class CharacterAdapter: BaseAdapter<Character>
+    {
+        readonly Activity context;
+        readonly List<Character> characters;
+
+        public CharacterAdapter(Activity context, List<Character> characters)
+        {
+            this.context = context;
+            this.characters = characters;
+        }
+
+        public override Character this[int position]
+        {
+            get
+            {
+                return this.characters[position];
+            }
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return this.characters.Count;
+            }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            //Cell reuse...
+            var view = convertView;
+            if (convertView == null)
+            {
+                view = this.context.LayoutInflater.Inflate(Resource.Layout.CharacterRow, parent, false);
+            }
+
+            var photo = view.FindViewById<ImageView>(Resource.Id.photoImageView);
+            var name = view.FindViewById<TextView>(Resource.Id.nameTextView);
+
+            if (this.characters[position].Thumbnail != null)
+            {
+                UrlImageViewHelper.SetUrlDrawable(photo, this.characters[position].Thumbnail.StandardLargeUri);
+            }
+
+            name.Text = this.characters[position].Name;
+
+            return view;
+        }
+    }
+}
---
+++ Android/CustomListAdapter/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using CustomListAdapter.Helpers;
+using MarvelPortable;
+using MarvelPortable.Model;
+
+namespace CustomListAdapter
+{
+    [Activity(Label = "CustomListAdapter", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override async void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            MarvelClient client = new MarvelClient(Constants.ApiKey, Constants.PrivateKey);
+            CharacterResponse response = await client.GetCharactersForSeriesAsync(1067);
+            List<Character> list = response.Results.ToList();
+
+            ListView listViewComics = this.FindViewById<ListView>(Resource.Id.Comics);
+            listViewComics.Adapter = new CharacterAdapter(this, list);
+        }
+    }
+}
+

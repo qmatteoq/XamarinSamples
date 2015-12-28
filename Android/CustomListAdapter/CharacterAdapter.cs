a5d861532948d5f2111aaa05d8f06ba8c3d2741a
Matteo Pagani <info@qmatteoq.com>
2015-12-28 15:42:40 +0100

    Updated Android samples

--- Android/CustomListAdapter/CharacterAdapter.cs
+++ Android/CustomListAdapter/CharacterAdapter.cs
@@ -1,15 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-
 using Android.App;
-using Android.Content;
-using Android.Content.Res;
-using Android.Graphics.Drawables;
-using Android.OS;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Koush;

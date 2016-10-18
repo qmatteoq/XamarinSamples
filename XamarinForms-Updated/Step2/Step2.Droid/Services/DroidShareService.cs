4a470c08b2447a83dad63daeda76b7ce80d47014
Matteo Pagani <info@qmatteoq.com>
2016-10-18 14:47:11 +0200

    Added better assets

--- XamarinForms-Updated/Step2/Step2.Droid/Services/DroidShareService.cs
+++ XamarinForms-Updated/Step2/Step2.Droid/Services/DroidShareService.cs
@@ -11,21 +11,17 @@ namespace Step2.Droid.Services
         public void Share(string title, string url)
         {
             var intent = new Intent(Intent.ActionSend);
-
             intent.SetType("text/plain");
-
             intent.PutExtra(Intent.ExtraText, url);
 
             if (title != null)
-
+            {
                 intent.PutExtra(Intent.ExtraSubject, title);
+            }
 
             var chooserIntent = Intent.CreateChooser(intent, title);
-
             chooserIntent.SetFlags(ActivityFlags.ClearTop);
-
             chooserIntent.SetFlags(ActivityFlags.NewTask);
-
             Forms.Context.StartActivity(chooserIntent);
         }
     }

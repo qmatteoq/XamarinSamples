ce108e5ef2a2c5d3b49612be3cb31e3fd763eacf
Matteo Pagani <info@qmatteoq.com>
2017-10-28 14:41:36 +0300

    Moved from PCL to .NET Standard 2.0 libraries

---
+++ Shared/XamarinSample/Web/RssReader/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NewsLibrary.Services;
+using NewsLibrary.Models;
+
+namespace RssReader.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<FeedItem> NewsList = new List<FeedItem>();
+
+        public async Task OnGet()
+        {
+            RssService service = new RssService();
+            string url = "https://blogs.msdn.microsoft.com/appconsult/feed/";
+            NewsList = await service.GetNews(url);
+        }
+    }
+}
